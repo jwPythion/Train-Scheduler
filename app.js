97f3634c8d08725ccd0a9f05eda86e302670abbf
jwpythion <jaymesjohnson@me.com>
2018-06-27 15:38:25 -0700

    updated style and minor things

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-No9J69q9OiED4MmkbV1IzUwaI6jlbxw",
+    authDomain: "cbc-june.firebaseapp.com",
+    databaseURL: "https://cbc-june.firebaseio.com",
+    projectId: "cbc-june",
+    storageBucket: "cbc-june.appspot.com",
+    messagingSenderId: "447999332501"
+  };
+  firebase.initializeApp(config);
